3f13270ea5eef0f53efdd4f474e2983b33b697dd
rascadux <rascadux@gmail.com>
2024-03-14 22:12:21 +0100

    Pruebas ClubDeportivoTest terminadas, con Jacoco 100%

--- src/test/java/es/uma/clubdeportivo/ClubDeportivoTest.java
+++ src/test/java/es/uma/clubdeportivo/ClubDeportivoTest.java
@@ -1,6 +1,5 @@
 package es.uma.clubdeportivo;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,51 +11,50 @@ class ClubDeportivoTest {
     private ClubDeportivo club;
     private final String[] datos = { "Actividad 1", "Futbol", "20", "10", "50.0" };
 
-    private static final Grupo grupo;
-
-    static {
-        try {
-            grupo = new Grupo("Actividad 1", "Futbol", 20, 10, 50.0);
-        } catch (ClubException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private static Grupo grupo;
 
     @BeforeEach
     void setup() throws ClubException {
         club = new ClubDeportivo("Club de Prueba");
+        grupo = new Grupo("Actividad 1", "Futbol", 20, 10, 50.0);
     }
 
     @Test
-    @DisplayName("Test 1: anyadirActividad")
+    @DisplayName("Test constructor - numero de grupos invalidos")
+    void constructor_gruposInvalidos_lanzaExcepcion() {
+        assertThrows(ClubException.class, () -> new ClubDeportivo("Club de Prueba", 0));
+    }
+
+    @Test
+    @DisplayName("Test anyadirActividad(String[]) - Formato correcto")
     void anyadirActividad_actividad_actividadAnyadida() throws ClubException{
         club.anyadirActividad(datos);
         assertEquals(10, club.plazasLibres("Futbol"));
     }
 
     @Test
-    @DisplayName("Test 2: anyadirActividad - Formato incorrecto")
+    @DisplayName("Test anyadirActividad(String[]) - Formato incorrecto")
     void anyadirActividad_formatoIncorrecto_lanzaExcepcion(){
         String[] datosMod = { "Actividad 1", "Futbol", "veinte", "10", "50.0"};
         assertThrows(ClubException.class, () -> club.anyadirActividad(datosMod));
     }
 
     @Test
-    @DisplayName("Test 3: anyadirActividad - Grupo nulo")
+    @DisplayName("Test anyadirActividad(Grupo) - Grupo nulo")
     void anyadirActividad_actividadNula_lanzaExcepcion(){
         Grupo g = null;
         assertThrows(ClubException.class, () -> club.anyadirActividad(g));
     }
 
     @Test
-    @DisplayName("Test 4: anyadirActividad - Grupo nuevo")
+    @DisplayName("Test anyadirActividad(Grupo) - Grupo nuevo")
     void anyadirActividad_grupoNuevo_grupoAnyadido() throws ClubException {
         club.anyadirActividad(grupo);
         assertEquals(10, club.plazasLibres("Futbol"));
     }
 
     @Test
-    @DisplayName("Test 5: anyadirActividad - Grupo existente")
+    @DisplayName("Test anyadirActividad(Grupo) - Grupo existente")
     void anyadirActividad_grupoExistente_grupoAnyadido() throws ClubException {
         club.anyadirActividad(grupo);
         Grupo g2 = new Grupo("Actividad 1", "Futbol", 30, 10, 50.0);
@@ -65,13 +63,22 @@ class ClubDeportivoTest {
     }
 
     @Test
-    @DisplayName("Test 6: plazasLibres - Actividad no existente")
+    @DisplayName("Test anyadirActividad(Grupo) - sin espacio")
+    void anyadirActividad_sinEspacio_lanzaExcepcion() throws ClubException {
+        ClubDeportivo clubMax = new ClubDeportivo("Club de Prueba Max", 1);
+        clubMax.anyadirActividad(grupo);
+        Grupo g2 = new Grupo("Actividad 2", "Baloncesto", 30, 10, 50.0);
+        assertThrows(ClubException.class, () -> clubMax.anyadirActividad(g2));
+    }
+
+    @Test
+    @DisplayName("Test plazasLibres - Actividad no existente")
     void plazasLibres_actividadNoExistente_ceroPlazasLibres() {
         assertEquals(0, club.plazasLibres("zzzzz"));
     }
 
     @Test
-    @DisplayName("Test 7: plazasLibres - Actividad existente")
+    @DisplayName("Test plazasLibres - Actividad existente")
     void plazasLibres_actividadExistente_plazasLibres() throws ClubException {
         Grupo g = new Grupo("Actividad 1", "Futbol", 20, 10, 50.0);
         club.anyadirActividad(g);
@@ -79,30 +86,42 @@ class ClubDeportivoTest {
     }
 
     @Test
-    @DisplayName("Test 8: matricular - Actividad no existente")
-    void matricular_actividadNoExistente_devuelveExcepcion() {
+    @DisplayName("Test matricular - Actividad no existente")
+    void matricular_actividadNoExistente_devuelveExcepcion(){
         assertThrows(ClubException.class, () -> club.matricular("zzzzz", 5));
     }
 
     @Test
-    @DisplayName("Test 9: matricular - Plazas insuficientes")
-    void matricular_plazasInsuficientes_devuelveExcepcion() throws ClubException {
-        Grupo g = new Grupo("Actividad 1", "Futbol", 20, 10, 50.0);
+    @DisplayName("Test matricular - Cero personas en ninguna plaza disponible")
+    void matricular_ceroPersonasEnCeroPlazas_sinCambios() throws ClubException {
+        Grupo g = new Grupo("Actividad 1", "Futbol", 20, 20, 50.0);
         club.anyadirActividad(g);
-        assertThrows(ClubException.class, () -> club.matricular("Futbol", 15));
+        club.matricular("Futbol", 0);
+        assertEquals(0, club.plazasLibres("Futbol"));
     }
 
     @Test
-    @DisplayName("Test 10: matricular")
-    void matricular_matriculado_matriculado() throws ClubException {
-        Grupo g = new Grupo("Actividad 1", "Futbol", 20, 10, 50.0);
+    @DisplayName("Test matricular - Plazas insuficientes en primer grupo, reparte en segundo grupo")
+    void matricular_plazasInsuficientesPrimerGrupo_matriculaEnSegundo() throws ClubException {
+        Grupo g = new Grupo("Actividad 2", "Futbol", 20, 10, 50.0);
+        club.anyadirActividad(grupo);
         club.anyadirActividad(g);
-        club.matricular("Futbol", 5);
+        club.matricular("Futbol", 15);
         assertEquals(5, club.plazasLibres("Futbol"));
     }
 
     @Test
-    @DisplayName("Test 11: ingresos")
+    @DisplayName("Test matricular - Datos correctos")
+    void matricular_matriculado_matriculado() throws ClubException {
+        club.anyadirActividad(grupo);
+        Grupo g = new Grupo("Actividad 1", "Baloncesto", 20, 10, 50.0);
+        club.anyadirActividad(g);
+        club.matricular("Baloncesto", 5);
+        assertEquals(5, club.plazasLibres("Baloncesto"));
+    }
+
+    @Test
+    @DisplayName("Test ingresos")
     void ingresos_ingresosCalculados_devuelveIngresos() throws ClubException {
         Grupo g = new Grupo("Actividad 1", "Futbol", 20, 10, 50.0);
         club.anyadirActividad(g);
@@ -111,7 +130,7 @@ class ClubDeportivoTest {
     }
 
     @Test
-    @DisplayName("Test 12: toString")
+    @DisplayName("Test toString")
     void toString_datosClub_devuelveToString() throws ClubException {
         Grupo g = new Grupo("Actividad 1", "Futbol", 20, 10, 50.0);
         club.anyadirActividad(g);
